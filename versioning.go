ff574c54fa2301650247b077a3c76f199402a82f
Chanwit Kaewkasi <chanwit@gmail.com>
2018-07-15 16:41:26 +0700

    Update versioning.go

--- versioning.go
+++ versioning.go
@@ -29,7 +29,7 @@ func (e *VersioningExtension) Name() string {
 }
 
 func (e *VersioningExtension) Setup(s fnext.ExtServer) error {
-    s.AddMiddleware(&VersioningMiddleware{})
+    s.AddAPIMiddleware(&VersioningMiddleware{})
     return nil
 }
 
